39cffbb4558d64b0e8e77047b5757635e7214f8a
mostafa khaled osman <123234339+mostafa-png@users.noreply.github.com>
2024-03-05 10:04:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+
+var menubtn = document.getElementById('menubtn')
+var sidenav = document.getElementById('sidenav')
+var menu = document.getElementById('menu')
+sidenav.style.right = "-250px"
+menubtn.onclick= function(){
+    if(sidenav.style.right== "-250px"){
+        sidenav.style.right="0";
+        menu.src = "images/close.png" ;// 22:37
+
+    }
+    else {
+        sidenav.style.right = "-250px"
+        menu.src = "images/menu.png" ;// 22:37
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
